c4e9c4e5f6150de0e8e099aabb8d7239eaafb2fa
anqurvanillapy <anqurvanillapy@gmail.com>
2023-09-06 22:19:55 +0800

    Multi-Raft PoC works now
    Signed-off-by: anqurvanillapy <anqurvanillapy@gmail.com>

---
+++ internal/groups/trans.go
@@ -0,0 +1,147 @@
+package groups
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/bocchi-the-cache/indeep/internal/logs"
+
+	"github.com/hashicorp/raft"
+)
+
+type (
+	NodeAddress string
+	GroupID     string
+)
+
+type StreamLayerMux interface {
+	NetworkLayer(groupID GroupID) raft.StreamLayer
+}
+
+type streamLayerMux struct {
+	localAddr NodeAddress
+	listener  *net.TCPListener
+	groups    map[GroupID]chan net.Conn
+	groupsMu  sync.RWMutex
+}
+
+func NewStreamLayerMux(localAddr NodeAddress) (StreamLayerMux, error) {
+	l, err := net.Listen("tcp", string(localAddr))
+	if err != nil {
+		return nil, err
+	}
+	m := &streamLayerMux{
+		localAddr: localAddr,
+		listener:  l.(*net.TCPListener),
+		groups:    make(map[GroupID]chan net.Conn),
+	}
+	go m.acceptMux()
+	return m, nil
+}
+
+func (m *streamLayerMux) acceptMux() {
+	for {
+		conn, err := m.listener.Accept()
+		if err != nil {
+			logs.S.Error("accept error", "err", err)
+			continue
+		}
+		groupID, err := readDialHeader(conn)
+		if err != nil {
+			logs.S.Error("read dial header error", "err", err)
+			continue
+		}
+		acceptor, ok := m.getAcceptor(groupID)
+		if !ok {
+			logs.S.Error("unknown acceptor", "groupID", groupID)
+			continue
+		}
+		acceptor <- conn
+	}
+}
+
+func (m *streamLayerMux) getAcceptor(groupID GroupID) (chan net.Conn, bool) {
+	m.groupsMu.RLock()
+	defer m.groupsMu.RUnlock()
+	ch, ok := m.groups[groupID]
+	return ch, ok
+}
+
+func (m *streamLayerMux) NetworkLayer(groupID GroupID) raft.StreamLayer {
+	return &streamLayer{
+		localAddr: m.localAddr,
+		groupID:   groupID,
+		mux:       m,
+		acceptor:  m.newAcceptor(groupID),
+	}
+}
+
+func (m *streamLayerMux) newAcceptor(groupID GroupID) chan net.Conn {
+	m.groupsMu.Lock()
+	defer m.groupsMu.Unlock()
+	if ch, ok := m.groups[groupID]; ok {
+		close(ch)
+	}
+	ch := make(chan net.Conn, 1)
+	m.groups[groupID] = ch
+	return ch
+}
+
+type streamLayer struct {
+	localAddr NodeAddress
+	groupID   GroupID
+	mux       *streamLayerMux
+	acceptor  <-chan net.Conn
+}
+
+func readDialHeader(r io.Reader) (GroupID, error) {
+	lenBuf := make([]byte, 1)
+	if _, err := r.Read(lenBuf); err != nil {
+		return "", err
+	}
+	l := int(lenBuf[0])
+	if l <= 0 {
+		return "", fmt.Errorf("invalid group ID length %q", l)
+	}
+	groupID := make([]byte, l)
+	if _, err := r.Read(groupID); err != nil {
+		return "", err
+	}
+	return GroupID(groupID), nil
+}
+
+func writeDialHeader(w io.Writer, groupID GroupID) error {
+	_, err := w.Write(append([]byte{byte(len(groupID))}, []byte(groupID)...))
+	return err
+}
+
+func trimGroupID(addr raft.ServerAddress) string {
+	s := string(addr)
+	return s[:strings.Index(s, "/")]
+}
+
+func (*streamLayer) Network() string  { return "tcp" }
+func (s *streamLayer) String() string { return fmt.Sprintf("%s/%s", s.localAddr, s.groupID) }
+
+func (s *streamLayer) Accept() (net.Conn, error) {
+	return <-s.acceptor, nil
+}
+
+func (*streamLayer) Close() error { return nil }
+
+func (s *streamLayer) Addr() net.Addr { return s }
+
+func (s *streamLayer) Dial(addr raft.ServerAddress, timeout time.Duration) (net.Conn, error) {
+	conn, err := net.DialTimeout(s.Network(), trimGroupID(addr), timeout)
+	if err != nil {
+		return nil, err
+	}
+	if err := writeDialHeader(conn, s.groupID); err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
---
+++ internal/groups/trans_test.go
@@ -0,0 +1,102 @@
+package groups
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/bocchi-the-cache/indeep/internal/logs"
+	"github.com/hashicorp/raft"
+	raftboltdb "github.com/hashicorp/raft-boltdb"
+)
+
+func newStreamLayerMux(t *testing.T, local NodeAddress) StreamLayerMux {
+	m, err := NewStreamLayerMux(local)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return m
+}
+
+func newBoltDB(t *testing.T, name string, id raft.ServerID) *raftboltdb.BoltStore {
+	id = raft.ServerID(strings.ReplaceAll(string(id), "/", "_"))
+	db, err := raftboltdb.New(raftboltdb.Options{Path: fmt.Sprintf("%s.%s.bolt", name, id)})
+	if err != nil {
+		t.Fatal(err)
+	}
+	return db
+}
+
+func bootstrapNode(t *testing.T, s raft.StreamLayer, c raft.Configuration, id raft.ServerID) {
+	config := raft.DefaultConfig()
+	config.LocalID = id
+	snaps, err := raft.NewFileSnapshotStoreWithLogger(".", 3, logs.HcLogger("snaps"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	trans := raft.NewNetworkTransportWithLogger(s, 10, 15*time.Second, logs.HcLogger("trans"))
+	r, err := raft.NewRaft(
+		config,
+		new(raft.MockFSM),
+		newBoltDB(t, "log", id),
+		newBoltDB(t, "stable", id),
+		snaps,
+		trans,
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	r.BootstrapCluster(c)
+	go func() {
+		time.Sleep(10 * time.Second)
+		leader, leaderID := r.LeaderWithID()
+		logs.S.Info("OHHHHHHHHHHHHHHH", "leader", leader, "leaderID", leaderID, "c", r.GetConfiguration().Configuration())
+	}()
+}
+
+func TestNewTransMux(t *testing.T) {
+	c1 := raft.Configuration{Servers: []raft.Server{
+		{ID: "n1/g1", Address: "127.0.0.1:10001/g1"},
+		{ID: "n2/g1", Address: "127.0.0.1:10002/g1"},
+		{ID: "n3/g1", Address: "127.0.0.1:10003/g1"},
+	}}
+	n1 := newStreamLayerMux(t, "127.0.0.1:10001")
+	n1g1 := n1.NetworkLayer("g1")
+	n1g2 := n1.NetworkLayer("g2")
+	n1g3 := n1.NetworkLayer("g3")
+
+	c2 := raft.Configuration{Servers: []raft.Server{
+		{ID: "n1/g2", Address: "127.0.0.1:10001/g2"},
+		{ID: "n2/g2", Address: "127.0.0.1:10002/g2"},
+		{ID: "n3/g2", Address: "127.0.0.1:10003/g2"},
+	}}
+	n2 := newStreamLayerMux(t, "127.0.0.1:10002")
+	n2g1 := n2.NetworkLayer("g1")
+	n2g2 := n2.NetworkLayer("g2")
+	n2g3 := n2.NetworkLayer("g3")
+
+	c3 := raft.Configuration{Servers: []raft.Server{
+		{ID: "n1/g3", Address: "127.0.0.1:10001/g3"},
+		{ID: "n2/g3", Address: "127.0.0.1:10002/g3"},
+		{ID: "n3/g3", Address: "127.0.0.1:10003/g3"},
+	}}
+	n3 := newStreamLayerMux(t, "127.0.0.1:10003")
+	n3g1 := n3.NetworkLayer("g1")
+	n3g2 := n3.NetworkLayer("g2")
+	n3g3 := n3.NetworkLayer("g3")
+
+	bootstrapNode(t, n1g1, c1, c1.Servers[0].ID)
+	bootstrapNode(t, n1g2, c1, c2.Servers[0].ID)
+	bootstrapNode(t, n1g3, c1, c3.Servers[0].ID)
+
+	bootstrapNode(t, n2g1, c2, c1.Servers[1].ID)
+	bootstrapNode(t, n2g2, c2, c2.Servers[1].ID)
+	bootstrapNode(t, n2g3, c2, c3.Servers[1].ID)
+
+	bootstrapNode(t, n3g1, c3, c1.Servers[2].ID)
+	bootstrapNode(t, n3g2, c3, c2.Servers[2].ID)
+	bootstrapNode(t, n3g3, c3, c3.Servers[2].ID)
+
+	select {}
+}
