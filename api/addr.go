7471f96d4e4b0b973cb635241dfb18ecf69c710a
anqurvanillapy <anqurvanillapy@gmail.com>
2023-09-12 18:26:22 +0800

    Address list
    Signed-off-by: anqurvanillapy <anqurvanillapy@gmail.com>

--- api/addr.go
+++ api/addr.go
@@ -2,13 +2,21 @@ package api
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"net/url"
+	"strings"
 )
 
 const (
 	HostScheme = "http"
 	RaftScheme = "tcp"
 	RootPath   = "/"
+	HostSep    = ","
+)
+
+var (
+	ErrAddressListEmptyHosts = errors.New("empty hosts in address list")
 )
 
 type RpcID string
@@ -19,17 +27,23 @@ type Addresser interface {
 
 type Address struct{ *url.URL }
 
+var (
+	_ = (fmt.Stringer)((*Address)(nil))
+	_ = (json.Marshaler)((*Address)(nil))
+	_ = (json.Unmarshaler)((*Address)(nil))
+)
+
 func NewAddress(scheme, host string) *Address {
 	return &Address{&url.URL{Scheme: scheme, Host: host}}
 }
 
-func (i *Address) RPC(id RpcID) *Address {
-	return &Address{&url.URL{Scheme: i.Scheme, Host: i.Host, Path: RootPath + string(id)}}
+func (a *Address) RPC(id RpcID) *Address {
+	return &Address{&url.URL{Scheme: a.Scheme, Host: a.Host, Path: RootPath + string(id)}}
 }
 
-func (i *Address) MarshalJSON() ([]byte, error) { return json.Marshal(i.String()) }
+func (a *Address) MarshalJSON() ([]byte, error) { return json.Marshal(a.String()) }
 
-func (i *Address) UnmarshalJSON(bytes []byte) error {
+func (a *Address) UnmarshalJSON(bytes []byte) error {
 	var rawURL string
 	if err := json.Unmarshal(bytes, &rawURL); err != nil {
 		return err
@@ -38,6 +52,51 @@ func (i *Address) UnmarshalJSON(bytes []byte) error {
 	if err != nil {
 		return err
 	}
-	i.URL = u
+	a.URL = u
 	return nil
+}
+
+type AddressList struct {
+	scheme string
+	hosts  []string
+}
+
+var (
+	_ = (fmt.Stringer)((*AddressList)(nil))
+	_ = (json.Marshaler)((*AddressList)(nil))
+	_ = (json.Unmarshaler)((*AddressList)(nil))
+)
+
+func (a *AddressList) String() string {
+	return (&url.URL{Scheme: a.scheme, Host: strings.Join(a.hosts, HostSep)}).String()
+}
+
+func (a *AddressList) MarshalJSON() ([]byte, error) { return json.Marshal(a.String()) }
+
+func (a *AddressList) UnmarshalJSON(bytes []byte) error {
+	var rawURL string
+	if err := json.Unmarshal(bytes, &rawURL); err != nil {
+		return err
+	}
+	scheme, hosts, err := parseAddressList(rawURL)
+	if err != nil {
+		return err
+	}
+	a.scheme = scheme
+	a.hosts = hosts
+	return nil
+}
+
+func parseAddressList(rawURL string) (scheme string, hosts []string, err error) {
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		return
+	}
+	if u.Host == "" {
+		err = ErrAddressListEmptyHosts
+		return
+	}
+	scheme = u.Scheme
+	hosts = strings.Split(u.Host, HostSep)
+	return
 }
